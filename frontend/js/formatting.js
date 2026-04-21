d58bb609ccc45047597fdb8aa3659a33dd3fe212
Etling-S <sevensoccersages@gmail.com>
2026-04-21 15:04:14 -0500

    Stock.html fix
    Stock so far

--- frontend/js/formatting.js
+++ frontend/js/formatting.js
@@ -5,3 +5,63 @@ function toggleSidebar() {
   // Toggles the 'active' class: adds it if missing, removes it if present
   sidebar.classList.toggle("active");
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*This is for the stock linechart in the current stock
+*/
+const ctx = document.getElementById('Stockchart');
+
+    const stockData = [50, 75, 60, 90, 120, 100, 80, 110];
+    const weeks = ["W1","W2","W3","W4","W5","W6","W7","W8"];
+
+    new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: weeks,
+        datasets: [{
+          label: 'Stock Units',
+          data: stockData,
+          borderWidth: 3,
+          fill: true, // this is the area fill beneath the lines
+          
+          pointRadius: 4
+        }]
+      },
+      options: {
+        responsive: true,
+        plugins: {
+          tooltip: {
+            enabled: true
+          },
+          legend: {
+            display: true
+          }
+        },
+        scales: {
+          x: {
+            title: {
+              display: true,
+              text: 'Weeks'
+            }
+          },
+          y: {
+            beginAtZero: true,
+            title: {
+              display: true,
+              text: 'Stock Units'
+            }
+          }
+        }
+      }
+    });
